7f3b57db28771aaa15ea790f36cdf81c9f43d40a
Minuli Kannangara <73284411+MinuliKannangara@users.noreply.github.com>
2023-10-02 22:50:38 +0530

    .

--- client/src/index.js
+++ client/src/index.js
@@ -46,8 +46,8 @@ const config = {
   GramaCheckApiUrl:
     "https://1adbbcb2-28ed-4caa-ace8-6191b640cb48-prod.e1-us-east-azure.choreoapis.dev/xqfp/user-details-service/user-details-617/v1",
   
-      signInRedirectURL: "https://9e9fd4c9-ccfc-43a0-969d-fe79e453fb3b.e1-us-east-azure.choreoapps.dev",
-      signOutRedirectURL: "https://9e9fd4c9-ccfc-43a0-969d-fe79e453fb3b.e1-us-east-azure.choreoapps.dev",
+      signInRedirectURL: "https://9e9fd4c9-ccfc-43a0-969d-fe79e453fb3b.e1-us-east-azure.choreoapps.dev/Page1",
+      signOutRedirectURL: "https://9e9fd4c9-ccfc-43a0-969d-fe79e453fb3b.e1-us-east-azure.choreoapps.dev/homePage",
       clientID: "bdQc9v1ho5B_lB1TbThXBfCywf8a",
       baseUrl: "https://api.asgardeo.io/t/orgnization1",
       scope: ["openid", "address", "email", "phone", "profile", "groups"]

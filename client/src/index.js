603aa4c9106499c3e37f86283fb2b9395d1a89f8
Minuli Kannangara <73284411+MinuliKannangara@users.noreply.github.com>
2023-10-02 22:25:42 +0530

    changed index.js

--- client/src/index.js
+++ client/src/index.js
@@ -47,13 +47,25 @@ function AuthRedirect() {
 //   baseUrl: "https://api.asgardeo.io/t/orgnization1",
 //   scope: ["openid", "address", "email", "phone", "profile", "groups"]
 // }
+// const config = {
+//   signInRedirectURL: window.config.signInRedirectURL,
+//   signOutRedirectURL: window.config.signOutRedirectURL,
+//   clientID: window.config.clientID,
+//   baseUrl: window.config.baseUrl,
+//   scope: ["openid", "address", "email", "phone", "profile", "groups"]
+// }
+
 const config = {
-  signInRedirectURL: window.config.signInRedirectURL,
-  signOutRedirectURL: window.config.signOutRedirectURL,
-  clientID: window.config.clientID,
-  baseUrl: window.config.baseUrl,
-  scope: ["openid", "address", "email", "phone", "profile", "groups"]
-}
+  GramaCheckApiUrl:
+    "https://1adbbcb2-28ed-4caa-ace8-6191b640cb48-prod.e1-us-east-azure.choreoapis.dev/xqfp/user-details-service/user-details-617/v1",
+  
+      signInRedirectURL: "https://9e9fd4c9-ccfc-43a0-969d-fe79e453fb3b.e1-us-east-azure.choreoapps.dev",
+      signOutRedirectURL: "https://9e9fd4c9-ccfc-43a0-969d-fe79e453fb3b.e1-us-east-azure.choreoapps.dev",
+      clientID: "bdQc9v1ho5B_lB1TbThXBfCywf8a",
+      baseUrl: "https://api.asgardeo.io/t/orgnization1",
+      scope: ["openid", "address", "email", "phone", "profile", "groups"]
+
+};
 
 
 
